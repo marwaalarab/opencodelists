bb424a3863a9835e0ec6c4b68d80191941836773
Becky Smith <becky.smith@thedatalab.org>
2022-11-08 09:06:27 +0000

    Pass on coding system version kwargs in import_data management command

--- opencodelists/management/commands/import_data.py
+++ opencodelists/management/commands/import_data.py
@@ -1,9 +1,14 @@
+import argparse
 import glob
 import os
 import sys
+from datetime import datetime
 from importlib import import_module
 
 from django.core.management import BaseCommand
+
+from codelists.coding_systems import CODING_SYSTEMS
+from coding_systems.versioning.models import CodingSystemRelease
 
 
 def iter_possible_modules():
@@ -14,23 +19,94 @@ def iter_possible_modules():
         yield head.replace(os.sep, ".")
 
 
+def valid_from(input_date):
+    try:
+        return datetime.strptime(input_date, "%Y%m%d")
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Not a valid date (YYYYMMDD): {input_date}")
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("dataset")
         parser.add_argument("release_dir")
+        parser.add_argument("--release", dest="release_name", help="Release name")
+        parser.add_argument(
+            "--valid-from",
+            type=valid_from,
+            help="For coding system imports: date the release is valid from, in YYYYMMDD format",
+        )
+        parser.add_argument(
+            "--import-ref",
+            help="For coding system imports: optional reference for this import",
+        )
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            help="Force an overwrite of a coding system release",
+        )
 
-    def handle(self, dataset, release_dir, **kwargs):
+    def handle(
+        self,
+        dataset,
+        release_dir,
+        release_name,
+        valid_from,
+        import_ref,
+        force,
+        **kwargs,
+    ):
         try:
             mod = import_module(dataset + ".import_data")
         except ModuleNotFoundError:
-            print(f"Could not find an 'import_data.py' module at path '{dataset}'")
-            print("Maybe you meant one of these module paths?")
+            self.stdout.write(
+                f"Could not find an 'import_data.py' module at path '{dataset}'"
+            )
+            self.stdout.write("Maybe you meant one of these module paths?")
 
             possibilities = list(iter_possible_modules())
             for possibility in possibilities:
-                print(possibility)
+                self.stdout.write(possibility)
 
             sys.exit(1)
 
+        module_name = dataset.rsplit(".", 1)[-1]
+        if module_name in CODING_SYSTEMS:
+            import_data_kwargs = self.get_coding_system_kwargs(
+                module_name, release_name, valid_from, import_ref, force
+            )
+        else:
+            import_data_kwargs = {}
+
         fn = getattr(mod, "import_data")
-        fn(release_dir)
+        fn(release_dir, **import_data_kwargs)
+
+    def get_coding_system_kwargs(
+        self, coding_system, release_name, valid_from, import_ref, force
+    ):
+        if not all([release_name, valid_from]):
+            self.stdout.write(
+                f"--release and --valid-from are required when importing {coding_system} data"
+            )
+            sys.exit(1)
+        else:
+            if (
+                CodingSystemRelease.objects.filter(
+                    coding_system=coding_system,
+                    release_name=release_name,
+                    valid_from=valid_from,
+                ).exists()
+                and not force
+            ):
+                self.stdout.write(
+                    f"A coding system release already exists for {coding_system} with release '{release_name}' and "
+                    f"valid from date {valid_from.strftime('%Y%m%d')}. Use the --force option to overwrite "
+                    "an existing release"
+                )
+                sys.exit(1)
+
+        return {
+            "release_name": release_name,
+            "valid_from": valid_from,
+            "import_ref": import_ref,
+        }
---
+++ opencodelists/tests/test_import_data.py
@@ -0,0 +1,143 @@
+import subprocess
+from datetime import date
+from unittest.mock import patch
+
+import pytest
+from django.core.management import call_command
+
+from coding_systems.versioning.models import CodingSystemRelease
+
+
+def test_unknown_module(capsys, tmpdir):
+    with pytest.raises(SystemExit) as error:
+        call_command("import_data", "unknown.module", tmpdir)
+    assert error.value.code == 1
+    captured = capsys.readouterr()
+    assert (
+        "Could not find an 'import_data.py' module at path 'unknown.module'"
+        in captured.out
+    )
+
+
+@pytest.mark.parametrize(
+    "kwargs", [{}, {"release_name": "v1"}, {"valid_from": date(2022, 10, 1)}]
+)
+def test_coding_system_required_arguments(capsys, tmpdir, kwargs):
+    with pytest.raises(SystemExit) as error:
+        call_command("import_data", "coding_systems.bnf", tmpdir, **kwargs)
+    assert error.value.code == 1
+    captured = capsys.readouterr()
+    assert (
+        "--release and --valid-from are required when importing bnf data"
+        in captured.out
+    )
+
+
+@patch("coding_systems.bnf.import_data.import_data")
+def test_calls_import_data_function_coding_system_import(mock_import_data, tmpdir):
+    call_command(
+        "import_data",
+        "coding_systems.bnf",
+        tmpdir,
+        release_name="version 1 A",
+        valid_from=date(2022, 10, 1),
+    )
+    mock_import_data.assert_called_once()
+    mock_import_data.assert_called_with(
+        str(tmpdir),
+        release_name="version 1 A",
+        valid_from=date(2022, 10, 1),
+        import_ref=None,
+    )
+
+
+@patch("mappings.bnfdmd.import_data.import_data")
+def test_calls_import_data_function_non_coding_system_import(mock_import_data, tmpdir):
+    # A non-coding system import doesn't require the extra args
+    call_command("import_data", "mappings.bnfdmd", tmpdir)
+    mock_import_data.assert_called_once()
+    mock_import_data.assert_called_with(str(tmpdir))
+
+
+def test_calls_import_data_function_coding_system_release_already_exists(
+    tmpdir, capsys
+):
+    CodingSystemRelease.objects.create(
+        coding_system="dmd",
+        release_name="v1",
+        valid_from=date(2022, 10, 1),
+    )
+    with pytest.raises(SystemExit) as error:
+        call_command(
+            "import_data",
+            "coding_systems.dmd",
+            tmpdir,
+            release_name="v1",
+            valid_from=date(2022, 10, 1),
+        )
+    assert error.value.code == 1
+    captured = capsys.readouterr()
+    assert (
+        "A coding system release already exists for dmd with release 'v1' and valid from date 20221001"
+        in captured.out
+    )
+
+
+@patch("coding_systems.snomedct.import_data.import_data")
+def test_calls_import_data_function_coding_system_force_overwrite(
+    mock_import_data, tmpdir
+):
+    CodingSystemRelease.objects.create(
+        coding_system="snomedct",
+        release_name="v1",
+        valid_from=date(2022, 10, 1),
+        import_ref="A first ref",
+    )
+
+    call_command(
+        "import_data",
+        "coding_systems.snomedct",
+        tmpdir,
+        release_name="v1",
+        valid_from=date(2022, 10, 1),
+        import_ref="A new ref",
+        force=True,
+    )
+
+    mock_import_data.assert_called_once()
+    mock_import_data.assert_called_with(
+        str(tmpdir),
+        release_name="v1",
+        valid_from=date(2022, 10, 1),
+        import_ref="A new ref",
+    )
+
+
+@pytest.mark.parametrize(
+    "input_date,returncode,error",
+    [
+        ("2020-1", 2, "Not a valid date (YYYYMMDD): 2020-1"),
+        ("2020-11-01", 2, "Not a valid date (YYYYMMDD): 2020-11-01"),
+        ("Nov", 2, "Not a valid date (YYYYMMDD): Nov"),
+        (("20200231", 2, "Not a valid date (YYYYMMDD): 20200231")),
+    ],
+)
+def test_valid_from_validation(capfd, input_date, returncode, error):
+    ret = subprocess.run(
+        [
+            "python",
+            "manage.py",
+            "import_data",
+            "coding_systems.snomedct",
+            "/tmp",
+            "--release",
+            "v1",
+            "--valid-from",
+            input_date,
+        ],
+        check=False,
+    )
+    assert ret.returncode == returncode
+    cap = capfd.readouterr()
+    assert error in cap.err
+    assert cap.out == ""
